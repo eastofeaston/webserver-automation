1acf17c82ec0677017c31348512824b9628e8afc
eastofeaston <rye.easton@gmail.com>
2025-02-24 03:07:38 -0700

    Switch to json config rather than confusing mix

--- build.sh
+++ build.sh
@@ -10,22 +10,16 @@ function usage {
 
 		$0: A tool to build the ryeeaston.com ci/cd infrastructure on Docker.
 
-		usage: $0  org_name repo_name [nickname]
-		       owner_name: the owner of a repository
-		       repo_name: the name of a repository
-		       nickname: (optional) the nickname for the machine this runs on, defaults to malus 
-		
-		environment:
-		       GITHUB_TOKEN: required, a github classic token
-		       RUNNERVER: optional, a custom github/runner version. defaults to 2.322.0
-			   IMGNAME: optional, a custom name for the ci/cd images. defaults to ryeeastoncom
+		usage: $0 config_file
+		       config_file: a JSON configuration file. Please see sample JSON config for
+		         guidance on what needs to be set.
 		
 		EOF
 }
 
-function chk_ghp {
-	if [ -z "$GITHUB_TOKEN" ]; then
-		echo "GITHUB_TOKEN not set. Please authorize a classic personal access token and export it."
+function set_conf {
+	if CONFIG="$(! cat "$1" | jq)"; then
+		echo "Config failed to be read!"
 		exit 2
 	fi
 }
@@ -38,6 +32,7 @@ function chk_dep {
 }
 
 function set_regtoken {
+	echo "Acquiring registration token..."
 	if ! REGTOKEN="$(curl -s -L -X POST \
 				-H "Accept: application/vnd.github+json" \
 				-H "Authorization: Bearer $GITHUB_TOKEN" \
@@ -48,6 +43,37 @@ function set_regtoken {
 		echo "Setting registration token failed. Do you have permission on the repo?"
 		exit 4
 	fi
+	echo "Registration token acquired."
+}
+
+function set_nonzero {
+	printf -v "$1" "$2"
+	[ -n "${!1}" ]
+}
+
+function eval_from_config {
+	if ! set_nonzero "$1" "$(echo "${!3}" | jq -r ".[\"$2\"] // empty")"; then
+		echo "The value of $2 could not be set from the $3."
+		echo "Is $2 missing from your configuration file? See the sample."
+		exit 5
+	fi
+}
+
+function init_global_settings {
+	eval_from_config NICKNAME nickname CONFIG
+	eval_from_config "IMGNAME" "image-name" CONFIG
+	eval_from_config CI_SETTINGS ci-settings CONFIG
+}
+
+function init_ci_settings {
+	eval_from_config GITHUB_TOKEN github-token CI_SETTINGS
+	eval_from_config RUNNERVER runner-version CI_SETTINGS
+	eval_from_config TARGET target CI_SETTINGS
+	eval_from_config OWNER owner TARGET
+	eval_from_config REPO repo TARGET
+	unset TARGET
+	unset CI_SETTINGS
+	set_regtoken
 }
 
 function announce_build {
@@ -60,29 +86,27 @@ function announce_build {
 
 # EXECUTION BEGINS HERE
 
-if (( $# < 2 )) || (( $# > 3 )); then
+if (( $# != 1 )); then
 	usage
 	exit 1
 fi
 
 set -e
 
-chk_ghp
 chk_dep command
 chk_dep curl
 chk_dep docker
 chk_dep jq
 
-OWNER="$1"
-REPO="$2"
-NICKNAME="${3:-malus}"
-RUNNERVER="${RUNNERVER:-2.322.0}"
-IMGNAME="${IMGNAME:-ryeeastoncom}"
-
-set_regtoken
+set_conf "$1"
+init_global_settings
 
 announce_build CI
+init_ci_settings
+
 docker build -t "$IMGNAME"-ci docker/ci --build-arg RUNNERVER="$RUNNERVER" --build-arg OWNER="$OWNER" --build-arg REPO="$REPO" --build-arg REGTOKEN="$REGTOKEN" --build-arg NICKNAME="$NICKNAME" 
 
 # announce_build CD
 # TO-DO
+
+echo "OK"
